58a393fd7530465eca761b612a60bb3b1b41ecc4
datoncito <campitos@mexico-movil.com>
2014-10-10 12:16:40 -0500

    miau

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/*
 var model={
-    user:'Adam',
+    user:'Campitos Ley',
     items:[
         { action:'Comprar Flores', done:false},
         {action:'Comprar papos', done:false},
@@ -14,8 +16,35 @@ var model={
         
     ]
 };
+*/
+//aqui viene json
+
+var model={
+    user:"Campitos Ley"
+};
+
+
 
 var todoApp=angular.module('todoApp',[]);
+
+// el tan amado y esperado ajax, sorprendeme angularjs
+todoApp.run(function($http){
+   $http.get("http://localhost:8091/jelastic_campitos/servicios/todo").success(function(data){
+     model.items=data;
+   });
+});
+
+todoApp.filter("checkedItems",function(){
+    return function(items,showComplete){
+        var resultArr=[];
+        angular.forEach(items, function(item){
+            if(item.done==false ||showComplete==true) {
+                resultArr.push(item);
+            }
+        });
+        return resultArr;
+    }
+});
 
 var modeloUsuario={
     items:[
@@ -26,6 +55,22 @@ var modeloUsuario={
 }
 todoApp.controller("ToDoCtrl",function($scope){
     $scope.todita=model;
+    $scope.usuarios=modeloUsuario;
+    //crecera el controller
+    $scope.incompleteCount=function(){
+        var count=0;
+        angular.forEach($scope.todita.items,function(item){
+            if(!item.done){count++}
+        });
+        return count;
+    }
+
+    $scope.warningLevel=function(){
+        return $scope.incompleteCount()< 3 ? "label-success": "label-warning"
+    }
+    $scope.addNewItem=function(actionText){
+        $scope.todita.items.push({action:actionText,done:false})
+    };
 
 });
 
